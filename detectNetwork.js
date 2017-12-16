7232e550c53315ffc599a11445c3f8ee3c1435a0
David Seidenberg <davidseidenberg@gmail.com>
2017-12-15 20:22:50 -0500

    Modify test suite and expand detectNetwork

--- detectNetwork.js
+++ detectNetwork.js
@@ -9,18 +9,23 @@
 
 var detectNetwork = function(cardNumber) {
   var prefix = cardNumber.slice(0,2);
+  
+  //Diner's Club
   if ((prefix == '38' || prefix == '39') && cardNumber.length == 14) {
     return "Diner's Club";
   }
   
+  // Amex
   if ((prefix == '34' || prefix == '37') && cardNumber.length == 15) {
     return 'American Express';
   }
   
+  // Visa
   if ((cardNumber.slice(0,1) == '4') && (cardNumber.length == 13 || cardNumber.length == 16 ||cardNumber.length == 19)) {
     return "Visa";
   }
   
+  // Mastercard
   if ((prefix == '51' ||
       prefix == '52' ||
       prefix == '53' ||
@@ -28,6 +33,28 @@ var detectNetwork = function(cardNumber) {
       prefix == '55') && cardNumber.length == 16) {
         return "MasterCard";
   }
+  
+  // Discover
+  if ((cardNumber.slice(0,4) == '6011' ||
+      cardNumber.slice(0,3) == '644' ||
+      cardNumber.slice(0,3) == '645' ||
+      cardNumber.slice(0,3) == '646' ||
+      cardNumber.slice(0,3) == '647' ||
+      cardNumber.slice(0,3) == '648' ||
+      cardNumber.slice(0,3) == '649' || 
+      cardNumber.slice(0,2) == '65') && (cardNumber.length == 16 || cardNumber.length == 19)) {
+    return "Discover";
+  }
+  
+  // Maestro
+  if ((12 <= cardNumber.length && cardNumber.length <= 19) &&
+     (cardNumber.slice(0,4) == '5018' ||
+     cardNumber.slice(0,4) == '5020' ||
+     cardNumber.slice(0,4) == '5038' ||
+     cardNumber.slice(0,4) == '6304')) {
+       return 'Maestro';
+  }
+  
   
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
